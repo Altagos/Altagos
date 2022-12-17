ada4975b4c00769799653234c3d51895ad0b30bd
Altagos <Altagos@users.noreply.github.com>
2022-12-17 16:51:29 +0000

    deploy: 63a4c55983f203d9b1957332798331c46b6ee7a2

--- main-f593c718f191a179.js
+++ main-f593c718f191a179.js
@@ -933,7 +933,7 @@ function initSync(bytes) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('main-ec75298879259713_bg.wasm', import.meta.url);
+        input = new URL('main-f593c718f191a179_bg.wasm', import.meta.url);
     }
     const imports = getImports();
 
